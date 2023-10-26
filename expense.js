7ecbb637b1a5086aa51b28807765fcf0dab31f8c
KNiC.xo <ntcaliwag@hotmail.com>
2023-10-26 01:07:35 -0700

    Removed HTML, added expenseArray and drawTable()

--- expense.js
+++ expense.js
@@ -4,32 +4,64 @@ const expenseDateInput = document.getElementById('expense-date');
 const submitBtn = document.getElementById('submit');
 const table = document.getElementById('table');
 
+let expenseArray = [{
+  name: 'Car Wash',
+  cost: '20',
+  date: '10/02/2023'
+}, {
+  name: 'Discord Nitro Classic',
+  cost:'4.99',
+  date: '10/10/2023'
+}];
+
 expenseNameInput.value = '';
 expenseCostInput.value = '';
 expenseDateInput.value = '';
 
+drawTable();
+
 submitBtn.addEventListener('click', addExpense);
+
+function drawTable() {
+  expenseArray.forEach((entry) => {
+    const tableEntry = document.createElement('tr');
+
+    tableEntry.innerHTML = `
+    <td>${entry.name}</td>
+    <td>${entry.cost}</td>
+    <td>${entry.date}</td>`;
+
+    table.append(tableEntry);
+  });
+}
 
 function addExpense() {
   if (expenseNameInput.value == '' || expenseCostInput.value == '' || expenseDateInput.value == '' || isNaN(expenseCostInput.value)) {
-    console.log(isNaN(expenseCostInput));
     expenseNameInput.value = '';
     expenseCostInput.value = '';
     expenseDateInput.value = '';
     return;
   }
 
-  const expenseName = expenseNameInput.value;
-  const expenseCost = expenseCostInput.value;
-  const expenseDate = `${expenseDateInput.value.slice(5,7)}/${expenseDateInput.value.slice(8,10)}/${expenseDateInput.value.slice(0,4)}`;
-  const tableEntry = document.createElement('tr');
+  const name = expenseNameInput.value;
+  const cost = expenseCostInput.value;
+  const date = `${expenseDateInput.value.slice(5,7)}/${expenseDateInput.value.slice(8,10)}/${expenseDateInput.value.slice(0,4)}`;
 
-  tableEntry.innerHTML = `
-   <td>${expenseName}</td>
-   <td>${expenseCost}</td>
-   <td>${expenseDate}</td>`;
+  expenseArray.push({
+    name,
+    cost,
+    date
+  })
 
-  table.append(tableEntry);
+  table.innerHTML = 
+  `<tr>
+    <th>Expense</th>
+    <th>Cost</th>
+    <th>Date</th>
+  </tr>`;
+
+  drawTable();
+  expenseArray.forEach((index) => console.log(index));
 
   expenseNameInput.value = '';
   expenseCostInput.value = '';
