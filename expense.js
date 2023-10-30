020085121528be416636ce7b4ff00b4c9707b225
KNiC.xo <ntcaliwag@hotmail.com>
2023-10-30 04:34:43 -0700

    Expenses are sorted oldest to newest when inserted

--- expense.js
+++ expense.js
@@ -4,15 +4,23 @@ const expenseDateInput = document.getElementById('expense-date');
 const submitBtn = document.getElementById('submit');
 const table = document.getElementById('table');
 
+let sortType = 'sortOldest';
+
 let expenseArray = [{
   name: 'Car Wash',
   cost: '20',
-  date: '10/02/2023'
+  date: '08/02/2023'
 }, {
   name: 'Discord Nitro Classic',
   cost:'4.99',
-  date: '10/10/2023'
+  date: '10/12/2023'
+},{
+  name: 'Birthday Gift',
+  cost:'74.31',
+  date: '10/13/2023'
 }];
+
+console.log(expenseArray[1].date.slice(6));
 
 expenseNameInput.value = '';
 expenseCostInput.value = '';
@@ -35,6 +43,46 @@ function drawTable() {
   });
 }
 
+// 10/02/2023
+// 0123456789
+function insertOldest(name, cost, date) {
+    for (let i = 0; i < expenseArray.length; i++) {
+      if (date == expenseArray[i].date) {
+        //pass
+      } else if (Number(date.slice(6)) < Number(expenseArray[i].date.slice(6))){ // 2019 < 2023
+        expenseArray.splice(i, 0, {
+          name,
+          cost,
+          date
+        });
+        return;
+      } else if (Number(date.slice(6)) == Number(expenseArray[i].date.slice(6))) {
+        if (Number(date.slice(0,2)) < Number(expenseArray[i].date.slice(0,2))) { // 8 < 10
+          expenseArray.splice(i, 0, {
+            name,
+            cost,
+            date
+          });
+          return;
+        } else if (Number(date.slice(0,2)) == Number(expenseArray[i].date.slice(0,2))) {
+          if (Number(date.slice(3,5) < Number(expenseArray[i].date.slice(3,5)))) { // 11 < 12
+            expenseArray.splice(i, 0, {
+              name,
+              cost,
+              date
+            });
+            return;
+          }
+        }
+      }
+    }
+    expenseArray.push({
+      name,
+      cost,
+      date
+    });
+}
+
 function addExpense() {
   if (expenseNameInput.value == '' || expenseCostInput.value == '' || expenseDateInput.value == '' || isNaN(expenseCostInput.value)) {
     expenseNameInput.value = '';
@@ -46,12 +94,7 @@ function addExpense() {
   const name = expenseNameInput.value;
   const cost = expenseCostInput.value;
   const date = `${expenseDateInput.value.slice(5,7)}/${expenseDateInput.value.slice(8,10)}/${expenseDateInput.value.slice(0,4)}`;
-
-  expenseArray.push({
-    name,
-    cost,
-    date
-  })
+  insertOldest(name, cost, date);
 
   table.innerHTML = 
   `<tr>
