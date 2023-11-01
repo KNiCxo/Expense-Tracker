086ed79abecdfb860dd21a33a51a895d568f3f0f
KNiC.xo <ntcaliwag@hotmail.com>
2023-10-31 21:45:24 -0700

    Added documentation

--- expense.js
+++ expense.js
@@ -1,11 +1,16 @@
+// Elements for Expense Input and Submit Button
 const expenseNameInput = document.getElementById('expense-name');
 const expenseCostInput = document.getElementById('expense-cost');
 const expenseDateInput = document.getElementById('expense-date');
 const submitBtn = document.getElementById('submit');
+
+// Element for Expense Table
 const table = document.getElementById('table');
 
+// String for tracking how expenses are sorted
 let sortType = 'sortOldest';
 
+// Array to store list of expenses
 let expenseArray = [{
   name: 'Car Wash',
   cost: '20',
@@ -20,16 +25,16 @@ let expenseArray = [{
   date: '10/13/2023'
 }];
 
-console.log(expenseArray[1].date.slice(6));
-
+// Clear Expense Input forms and draw expense table
 expenseNameInput.value = '';
 expenseCostInput.value = '';
 expenseDateInput.value = '';
-
 drawTable();
 
+// Expense is added to table when Submit Button is clicked
 submitBtn.addEventListener('click', addExpense);
 
+// Displays all expenses stored in the array as a table
 function drawTable() {
   expenseArray.forEach((entry) => {
     const tableEntry = document.createElement('tr');
@@ -43,47 +48,56 @@ function drawTable() {
   });
 }
 
-// 10/02/2023
-// 0123456789
+// Inserts the expense into the array based on the "Oldest" sort order
 function insertOldest(name, cost, date) {
-    for (let i = 0; i < expenseArray.length; i++) {
-      if (date == expenseArray[i].date) {
-        //pass
-      } else if (Number(date.slice(6)) < Number(expenseArray[i].date.slice(6))){ // 2019 < 2023
+  // Iterates through entire array or until return statement
+  for (let i = 0; i < expenseArray.length; i++) {
+    // If dates match, ignore (This causes the expense to be inserted at the end of all matching dates)
+    if (date == expenseArray[i].date) {
+      //pass
+    } else if (Number(date.slice(6)) < Number(expenseArray[i].date.slice(6))){
+      // If entry year is less than element year, then insert in front of element
+      expenseArray.splice(i, 0, {
+        name,
+        cost,
+        date
+      });
+      return;
+    } else if (Number(date.slice(6)) == Number(expenseArray[i].date.slice(6))) {
+      // If entry year and element year are the same, then check if entry month is less than element month
+      if (Number(date.slice(0,2)) < Number(expenseArray[i].date.slice(0,2))) {
+        // If entry month is less than element month, then insert in front of element
         expenseArray.splice(i, 0, {
           name,
           cost,
           date
         });
         return;
-      } else if (Number(date.slice(6)) == Number(expenseArray[i].date.slice(6))) {
-        if (Number(date.slice(0,2)) < Number(expenseArray[i].date.slice(0,2))) { // 8 < 10
+      } else if (Number(date.slice(0,2)) == Number(expenseArray[i].date.slice(0,2))) {
+        // If entry month and element month are the same, then check if entry day is less than element day
+        if (Number(date.slice(3,5) < Number(expenseArray[i].date.slice(3,5)))) {
+          // If entry day is less than element day, then insert in front of element
           expenseArray.splice(i, 0, {
             name,
             cost,
             date
           });
           return;
-        } else if (Number(date.slice(0,2)) == Number(expenseArray[i].date.slice(0,2))) {
-          if (Number(date.slice(3,5) < Number(expenseArray[i].date.slice(3,5)))) { // 11 < 12
-            expenseArray.splice(i, 0, {
-              name,
-              cost,
-              date
-            });
-            return;
-          }
         }
       }
     }
-    expenseArray.push({
-      name,
-      cost,
-      date
-    });
+  }
+  // If array is iterated through entirely, then push entry to end of array
+  expenseArray.push({
+    name,
+    cost,
+    date
+  });
 }
 
+// Adds expense to array and table based on sort order
 function addExpense() {
+  // If any input forms are empty or expense cost is NaN, then return
   if (expenseNameInput.value == '' || expenseCostInput.value == '' || expenseDateInput.value == '' || isNaN(expenseCostInput.value)) {
     expenseNameInput.value = '';
     expenseCostInput.value = '';
@@ -91,21 +105,27 @@ function addExpense() {
     return;
   }
 
+  // Get expense name, cost, and date (formatted mm/dd/yyyy) from input forms
   const name = expenseNameInput.value;
   const cost = expenseCostInput.value;
   const date = `${expenseDateInput.value.slice(5,7)}/${expenseDateInput.value.slice(8,10)}/${expenseDateInput.value.slice(0,4)}`;
+
+  // Inserts entry into array based on sort order
   insertOldest(name, cost, date);
 
+  // Clears table and redraws with new entry
   table.innerHTML = 
   `<tr>
     <th>Expense</th>
     <th>Cost</th>
     <th>Date</th>
   </tr>`;
-
   drawTable();
+
+  // *** TEMPORARY CODE: DELETE LATER *** \\
   expenseArray.forEach((index) => console.log(index));
 
+  // Clears input forms
   expenseNameInput.value = '';
   expenseCostInput.value = '';
   expenseDateInput.value = '';
