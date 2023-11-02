d1cae5a541a16925c66040959b234f1860d3a0f9
KNiC.xo <ntcaliwag@hotmail.com>
2023-11-02 03:47:49 -0700

    Sort Feature WIP * Added sorting selection HTML * Implemented "Sort By: Newest" feature * Created empty sort functions * Changed "Insert" functions to have array parameter * Added clearTable() function * Renamed primary files to expenseTracker.(ext) * Added "expenses.js" to contain expenseArray and inserting/sorting functions

--- expense.js
+++ expense.js
@@ -1,83 +1,67 @@
-// Elements for Expense Input and Submit Button
-const expenseNameInput = document.getElementById('expense-name');
-const expenseCostInput = document.getElementById('expense-cost');
-const expenseDateInput = document.getElementById('expense-date');
-const submitBtn = document.getElementById('submit');
-
-// Element for Expense Table
-const table = document.getElementById('table');
-
-// String for tracking how expenses are sorted
-let sortType = 'least-expensive';
-
 // Array to store list of expenses
-let expenseArray = [{
-    name: 'Discord Nitro Classic',
-    cost:'4.99',
-    date: '10/12/2023'
-  }, {
-    name: 'Car Wash',
-    cost: '20',
-    date: '08/02/2023'
-  }, {
+export let expenseArray = [{
+  name: 'Drugs',
+  cost: '30.44',
+  date: '04/02/2023'
+}, {
   name: 'Birthday Gift',
   cost:'74.31',
   date: '10/13/2023'
+}, {
+  name: 'Discord Nitro Classic',
+  cost:'4.99',
+  date: '10/12/2023'
+}, {
+name: 'Car Wash',
+cost: '20',
+date: '08/02/2023'
 }];
 
-// Clear Expense Input forms and draw expense table
-expenseNameInput.value = '';
-expenseCostInput.value = '';
-expenseDateInput.value = '';
-drawTable();
+// Inserts the expense into the array based on the "Newest" sort order
+export function insertNewest(name, cost, date, array) {
+  // If array is empty, then push entry into array
+  if (array.length == 0) {
+    array.push({
+      name,
+      cost,
+      date
+    });
+    return;
+  }
 
-// Expense is added to table when Submit Button is clicked
-submitBtn.addEventListener('click', addExpense);
-
-// Displays all expenses stored in the array as a table
-function drawTable() {
-  expenseArray.forEach((entry) => {
-    const tableEntry = document.createElement('tr');
-
-    tableEntry.innerHTML = `
-    <td>${entry.name}</td>
-    <td>${entry.cost}</td>
-    <td>${entry.date}</td>`;
-
-    table.append(tableEntry);
-  });
-}
-
-// Inserts the expense into the array based on the "Oldest" sort order
-function insertOldest(name, cost, date) {
   // Iterates through entire array or until return statement
-  for (let i = 0; i < expenseArray.length; i++) {
-    // If dates match, ignore (This causes the expense to be inserted at the end of all matching dates)
-    if (date == expenseArray[i].date) {
-      //pass
-    } else if (Number(date.slice(6)) < Number(expenseArray[i].date.slice(6))){
-      // If entry year is less than element year, then insert in front of element
-      expenseArray.splice(i, 0, {
+  for (let i = 0; i < array.length; i++) {
+    // If dates match, then insert in front of element
+    if (date == array[i].date) {
+      array.splice(i, 0, {
         name,
         cost,
         date
       });
       return;
-    } else if (Number(date.slice(6)) == Number(expenseArray[i].date.slice(6))) {
-      // If entry year and element year are the same, then check if entry month is less than element month
-      if (Number(date.slice(0,2)) < Number(expenseArray[i].date.slice(0,2))) {
-        // If entry month is less than element month, then insert in front of element
-        expenseArray.splice(i, 0, {
+    } else if (Number(date.slice(6)) > Number(array[i].date.slice(6))){
+      // If entry year is greater than element year, then insert in front of element
+      array.splice(i, 0, {
+        name,
+        cost,
+        date
+      });
+      return;
+    } else if (Number(date.slice(6)) == Number(array[i].date.slice(6))){
+      // If entry year and element year are the same, then check if entry month is greater than element month
+      if (Number(date.slice(0,2)) > Number(array[i].date.slice(0,2))) {
+        // If entry month is greater than element month, then insert in front of element
+        array.splice(i, 0, {
           name,
           cost,
           date
         });
         return;
-      } else if (Number(date.slice(0,2)) == Number(expenseArray[i].date.slice(0,2))) {
-        // If entry month and element month are the same, then check if entry day is less than element day
-        if (Number(date.slice(3,5) < Number(expenseArray[i].date.slice(3,5)))) {
-          // If entry day is less than element day, then insert in front of element
-          expenseArray.splice(i, 0, {
+      } else if (Number(date.slice(0,2)) == Number(array[i].date.slice(0,2))) {
+        // If entry month and element month are the same, then check if entry day is greater than element day
+        if (Number(date.slice(3,5) > Number(array[i].date.slice(3,5)))) {
+          // If entry day is greater than element day, then insert in front of element
+          array.splice(i, 0, {
             name,
             cost,
             date
@@ -88,48 +72,53 @@ function insertOldest(name, cost, date) {
     }
   }
   // If array is iterated through entirely, then push entry to end of array
-  expenseArray.push({
+  array.push({
     name,
     cost,
     date
   });
 }
 
-// Inserts the expense into the array based on the "Newest" sort order
-function insertNewest(name, cost, date) {
+// Inserts the expense into the array based on the "Oldest" sort order
+export function insertOldest(name, cost, date, array) {
+  // If array is empty, then push entry into array
+  if (array.length == 0) {
+    array.push({
+      name,
+      cost,
+      date
+    });
+    return;
+  }
+
   // Iterates through entire array or until return statement
-  for (let i = 0; i < expenseArray.length; i++) {
-    // If dates match, then insert in front of element
-    if (date == expenseArray[i].date) {
-      expenseArray.splice(i, 0, {
+  for (let i = 0; i < array.length; i++) {
+    // If dates match, ignore (This causes the expense to be inserted at the end of all matching dates)
+    if (date == array[i].date) {
+      //pass
+    } else if (Number(date.slice(6)) < Number(array[i].date.slice(6))){
+      // If entry year is less than element year, then insert in front of element
+      array.splice(i, 0, {
         name,
         cost,
         date
       });
       return;
-    } else if (Number(date.slice(6)) > Number(expenseArray[i].date.slice(6))){
-      // If entry year is greater than element year, then insert in front of element
-      expenseArray.splice(i, 0, {
-        name,
-        cost,
-        date
-      });
-      return;
-    } else if (Number(date.slice(6)) == Number(expenseArray[i].date.slice(6))){
-      // If entry year and element year are the same, then check if entry month is greater than element month
-      if (Number(date.slice(0,2)) > Number(expenseArray[i].date.slice(0,2))) {
-        // If entry month is greater than element month, then insert in front of element
-        expenseArray.splice(i, 0, {
+    } else if (Number(date.slice(6)) == Number(array[i].date.slice(6))) {
+      // If entry year and element year are the same, then check if entry month is less than element month
+      if (Number(date.slice(0,2)) < Number(array[i].date.slice(0,2))) {
+        // If entry month is less than element month, then insert in front of element
+        array.splice(i, 0, {
           name,
           cost,
           date
         });
         return;
-      } else if (Number(date.slice(0,2)) == Number(expenseArray[i].date.slice(0,2))) {
-        // If entry month and element month are the same, then check if entry day is greater than element day
-        if (Number(date.slice(3,5) > Number(expenseArray[i].date.slice(3,5)))) {
-          // If entry day is greater than element day, then insert in front of element
-          expenseArray.splice(i, 0, {
+      } else if (Number(date.slice(0,2)) == Number(array[i].date.slice(0,2))) {
+        // If entry month and element month are the same, then check if entry day is less than element day
+        if (Number(date.slice(3,5) < Number(array[i].date.slice(3,5)))) {
+          // If entry day is less than element day, then insert in front of element
+          array.splice(i, 0, {
             name,
             cost,
             date
@@ -140,7 +129,7 @@ function insertNewest(name, cost, date) {
     }
   }
   // If array is iterated through entirely, then push entry to end of array
-  expenseArray.push({
+  array.push({
     name,
     cost,
     date
@@ -148,12 +137,22 @@ function insertNewest(name, cost, date) {
 }
 
 // Inserts the expense into the array based on "Most Expensive" sort order
-function insertMostExpensive(name, cost, date) {
+export function insertMostExpensive(name, cost, date, array) {
+  // If array is empty, then push entry into array
+  if (array.length == 0) {
+    array.push({
+      name,
+      cost,
+      date
+    });
+    return;
+  }
+
   // Iterates through entire array or until return statement
-  for (let i = 0; i < expenseArray.length; i++) {
+  for (let i = 0; i < array.length; i++) {
     // If entry cost is greater than element cost, then insert in front of element
-    if (Number(cost) > Number(expenseArray[i].cost)) {
-      expenseArray.splice(i, 0, {
+    if (Number(cost) > Number(array[i].cost)) {
+      array.splice(i, 0, {
         name,
         cost,
         date
@@ -162,7 +161,7 @@ function insertMostExpensive(name, cost, date) {
     }
   }
   // If array is iterated through entirely, then push entry to end of array
-  expenseArray.push({
+  array.push({
     name,
     cost,
     date
@@ -170,12 +169,22 @@ function insertMostExpensive(name, cost, date) {
 }
 
 // Inserts the expense into the array based on "Least Expensive" sort order
-function insertLeastExpensive(name, cost, date) {
+export function insertLeastExpensive(name, cost, date, array) {
+  // If array is empty, then push entry into array
+  if (array.length == 0) {
+    array.push({
+      name,
+      cost,
+      date
+    });
+    return;
+  }
+
   // Iterates through entire array or until return statement
-  for (let i = 0; i < expenseArray.length; i++) {
+  for (let i = 0; i < array.length; i++) {
     // If entry cost is less than element cost, then insert in front of element
-    if (Number(cost) < Number(expenseArray[i].cost)) {
-      expenseArray.splice(i, 0, {
+    if (Number(cost) < Number(array[i].cost)) {
+      array.splice(i, 0, {
         name,
         cost,
         date
@@ -184,58 +193,31 @@ function insertLeastExpensive(name, cost, date) {
     }
   }
   // If array is iterated through entirely, then push entry to end of array
-  expenseArray.push({
+  array.push({
     name,
     cost,
     date
   });
 }
 
-// Adds expense to array and table based on sort order
-function addExpense() {
-  // If any input forms are empty or expense cost is NaN or negative, then return
-  if (expenseNameInput.value == '' || expenseCostInput.value == '' || expenseDateInput.value == '' || isNaN(expenseCostInput.value)) {
-    expenseNameInput.value = '';
-    expenseCostInput.value = '';
-    expenseDateInput.value = '';
-    return;
+export function sortNewest() {
+  let tempArray = [];
+  
+  for (let i = 0; i < expenseArray.length; i++) {
+    insertNewest(expenseArray[i].name, expenseArray[i].cost, expenseArray[i].date, tempArray);
   }
 
-  // Get expense name, cost, and date (formatted mm/dd/yyyy) from input forms
-  const name = expenseNameInput.value;
-  const cost = expenseCostInput.value;
-  const date = `${expenseDateInput.value.slice(5,7)}/${expenseDateInput.value.slice(8,10)}/${expenseDateInput.value.slice(0,4)}`;
+  expenseArray = tempArray;
+}
 
-  // Inserts entry into array based on sort order
-  switch (sortType) {
-    case 'oldest':
-      insertOldest(name, cost, date);
-      break;
-    case 'newest':
-      insertNewest(name, cost, date);
-      break;
-    case 'most-expensive':
-      insertMostExpensive(name, cost, date);
-      break;
-    case 'least-expensive':
-      insertLeastExpensive(name, cost, date);
-      break;
-  }
+export function sortOldest() {
 
-  // Clears table and redraws with new entry
-  table.innerHTML = 
-  `<tr>
-    <th>Expense</th>
-    <th>Cost</th>
-    <th>Date</th>
-  </tr>`;
-  drawTable();
+}
 
-  // *** TEMPORARY CODE: DELETE LATER *** \\
-  expenseArray.forEach((index) => console.log(index));
+export function sortMostExpensive() {
 
-  // Clears input forms
-  expenseNameInput.value = '';
-  expenseCostInput.value = '';
-  expenseDateInput.value = '';
+}
+
+export function sortLeastExpensive() {
+
 }
---
+++ expenseTracker.js
@@ -0,0 +1,119 @@
+import {
+  expenseArray, insertNewest, insertOldest, insertLeastExpensive, insertMostExpensive, 
+  sortNewest, sortOldest, sortLeastExpensive, sortMostExpensive
+} from "./expense.js";
+
+// Elements for Expense Input and Submit Button
+const expenseNameInput = document.getElementById('expense-name');
+const expenseCostInput = document.getElementById('expense-cost');
+const expenseDateInput = document.getElementById('expense-date');
+const submitBtn = document.getElementById('submit');
+
+// Element for Sort Menu
+const sortMenu = document.getElementById('select-sort');
+
+// Element for Expense Table
+const table = document.getElementById('table');
+
+// String for tracking how expenses are sorted
+let sortType = 'newest';
+
+// Clear Expense Input forms and draw expense table
+expenseNameInput.value = '';
+expenseCostInput.value = '';
+expenseDateInput.value = '';
+drawTable();
+
+// Expense is added to table when Submit Button is clicked
+submitBtn.addEventListener('click', addExpense);
+
+// Sorts array based on sort order when clicked
+sortMenu.addEventListener('click', () => {
+  sortType = sortMenu.value;
+
+  switch (sortType) {
+    case 'newest':
+      sortNewest();
+      break;
+    case 'oldest':
+      sortOldest();
+      break;
+    case 'most-expensive':
+      sortMostExpensive();
+      break;
+    case 'least-expensive':
+      sortLeastExpensive();
+      break;
+  }
+  
+  clearTable();
+  drawTable();
+});
+
+ // Clears table
+function clearTable() {
+  table.innerHTML = 
+  `<tr>
+    <th>Expense</th>
+    <th>Cost</th>
+    <th>Date</th>
+  </tr>`;
+}
+
+// Displays all expenses stored in the array as a table
+function drawTable() {
+  expenseArray.forEach((entry) => {
+    const tableEntry = document.createElement('tr');
+
+    tableEntry.innerHTML = `
+    <td>${entry.name}</td>
+    <td>${entry.cost}</td>
+    <td>${entry.date}</td>`;
+
+    table.append(tableEntry);
+  });
+}
+
+// Adds expense to array and table based on sort order
+function addExpense() {
+  // If any input forms are empty or expense cost is NaN or negative, then return
+  if (expenseNameInput.value == '' || expenseCostInput.value == '' || expenseDateInput.value == '' || isNaN(expenseCostInput.value)) {
+    expenseNameInput.value = '';
+    expenseCostInput.value = '';
+    expenseDateInput.value = '';
+    return;
+  }
+
+  // Get expense name, cost, and date (formatted mm/dd/yyyy) from input forms
+  const name = expenseNameInput.value;
+  const cost = expenseCostInput.value;
+  const date = `${expenseDateInput.value.slice(5,7)}/${expenseDateInput.value.slice(8,10)}/${expenseDateInput.value.slice(0,4)}`;
+
+  // Inserts entry into array based on sort order
+  switch (sortType) {
+    case 'newest':
+      insertNewest(name, cost, date, expenseArray);
+      break;
+    case 'oldest':
+      insertOldest(name, cost, date, expenseArray);
+      break;
+    case 'most-expensive':
+      insertMostExpensive(name, cost, date, expenseArray);
+      break;
+    case 'least-expensive':
+      insertLeastExpensive(name, cost, date, expenseArray);
+      break;
+  }
+
+  // Clears table and redraws with new entry
+  clearTable();
+  drawTable();
+
+  // *** TEMPORARY CODE: DELETE LATER *** \\
+  expenseArray.forEach((index) => console.log(index));
+
+  // Clears input forms
+  expenseNameInput.value = '';
+  expenseCostInput.value = '';
+  expenseDateInput.value = '';
+}
